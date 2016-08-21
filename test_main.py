d130fc6870c27b254cad0b4f8300be45918bbb6d
mick-warehime <mickwarehime@gmail.com>
2016-08-21 11:45:44 -0700

    add a unittest example file

---
+++ test_main.py
@@ -0,0 +1,21 @@
+import unittest
+import main
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_stupid(self):
+        exp = "hello warld"
+        act = main.stupid()
+        self.assertEqual(exp, act, "expected: %s, actual: %s" % (exp, act))
+
+    def test_dumb(self):
+        myname = "abc"
+        expected_name = "abc"
+        self.assertEqual(expected_name, myname, "expected: %s, actual: %s" % (expected_name, myname))
+
