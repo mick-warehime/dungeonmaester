4882dadc8b01db19047da63847ceca67a4c4faf0
mick-warehime <mickwarehime@gmail.com>
2016-08-23 22:07:00 -0700

    pep 8 node code

--- model/test_world_node.py
+++ model/test_world_node.py
@@ -1,8 +1,8 @@
 import unittest
 import world_node as wn
 
-class MainTestCase(unittest.TestCase):
 
+class MainTestCase(unittest.TestCase):
     def test_create_empty_node(self):
         n = wn.WorldNode()
         self.assertEqual(n.get_classifier(), wn.NODE_CLASSIFIERS.NONE, 'did not create an empty node')
@@ -10,5 +10,3 @@ class MainTestCase(unittest.TestCase):
     def test_create_NPC(self):
         n = wn.WorldNode(wn.NODE_CLASSIFIERS.NPC)
         self.assertEqual(n.get_classifier(), wn.NODE_CLASSIFIERS.NPC, 'created NPC successfully')
-
-
--- model/world_node.py
+++ model/world_node.py
@@ -1,11 +1,12 @@
 def enum(**enums):
     return type('Enum', (), enums)
 
-NODE_CLASSIFIERS = enum(NONE = 0, NPC = 1, CITY = 2)
+
+NODE_CLASSIFIERS = enum(NONE=0, NPC=1, CITY=2)
+
 
 class WorldNode():
-
-    def __init__(self, node_classifier = NODE_CLASSIFIERS.NONE):
+    def __init__(self, node_classifier=NODE_CLASSIFIERS.NONE):
         self.classifier = node_classifier
 
     def get_classifier(self):
