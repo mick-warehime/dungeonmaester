c35fe0a85c9600e25343f986c4932ccc4c875e90
Dvir Kafri <dvir.kafri@gmail.com>
2016-08-24 21:25:54 -0700

    Started graph edge tests.

--- model/graph.py
+++ model/graph.py
@@ -1,13 +1,27 @@
+# coding=utf-8
 """ Module for graph representing the game world."""
 
 
 class Node(object):
-    def __init__(self, description = None, edge_list = None):
+    """
+    Represents an object within the game world.
+
+    As components it should have a description, as well as a collection of relationships (edges) to other nodes. The
+    string method should spit the node’s description and its relationships. There should a method that outputs all nodes
+    that the given nodes has a `contains’ relationship with, as well as the node it’s `contained in’.
+
+    """
+
+    def __init__(self, description=None, edge_list=None):
         self.description = None
         self.edge_list = None
 
 
 class Edge(object):
+    CAT_PARENT_CHILD = 0
+    CAT_KNOWS = 1
+    CAT_HATES = 2
+
     def __init__(self, first_node, second_node, category, is_directed=False):
         self._first_node = first_node
         self._second_node = second_node
--- model/graph_test.py
+++ model/graph_test.py
@@ -16,3 +16,13 @@ class GraphTest(unittest.TestCase):
         edge = graph.Edge(None, None, None)
 
         self.assertTrue(not edge.is_directed(), "Expected False, got %s" % str(edge.is_directed()))
+
+    def test_edge_returns_nodes(self):
+        edge = graph.Edge(1, 2, None)
+
+        actual_left, actual_right = edge.get_nodes()
+        expected_left = 1
+        expected_right = 2
+
+        self.assertEqual(expected_left, actual_left, "Expected %d, got %d" % (expected_left, actual_left))
+        self.assertEqual(expected_right, actual_right, "Expected %d, got %d" % (expected_right, actual_right))
