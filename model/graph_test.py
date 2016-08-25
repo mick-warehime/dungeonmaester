ea2697d056133bd10374aa75998d6ea376efb08c
Dvir Kafri <dvir.kafri@gmail.com>
2016-08-25 06:18:29 -0700

    Added more tests, merged master. (#17)
    * Started graph edge tests.
    
    * Started graph edge tests.
    
    * Merge branch 'master' into graph
    
    Conflicts:
    	model/graph_test.py

--- model/graph_test.py
+++ model/graph_test.py
@@ -16,3 +16,16 @@ class GraphTest(unittest.TestCase):
         edge = graph.Edge(None, None, None)
 
         self.assertTrue(not edge.is_directed(), "Expected False, got %s" % str(edge.is_directed()))
+
+    def test_edge_returns_nodes(self):
+        node_1 = graph.Node()
+        node_2 = graph.Node()
+
+        edge = graph.Edge(node_1, node_2, None)
+
+        actual_left, actual_right = edge.get_nodes()
+        expected_left = node_1
+        expected_right = node_2
+
+        self.assertEqual(expected_left, actual_left, "Expected %s, got %s" % (str(expected_left), str(actual_left)))
+        self.assertEqual(expected_right, actual_right, "Expected %s, got %s" % (str(expected_right), str(actual_right)))
