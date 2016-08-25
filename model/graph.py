1e49d7669627fd08ac99a866a30829257e6fe2ce
Dvir Kafri <dvir.kafri@gmail.com>
2016-08-24 20:51:36 -0700

    Started graph module. Haven't figured out how to branch in pycharm so I am commiting to master.

---
+++ model/graph.py
@@ -0,0 +1,21 @@
+""" Module for graph representing the game world."""
+
+
+class Node(object):
+    def __init__(self, description = None, edge_list = None):
+        self.description = None
+        self.edge_list = None
+
+
+class Edge(object):
+    def __init__(self, first_node, second_node, category, is_directed=False):
+        self._first_node = first_node
+        self._second_node = second_node
+        self._category = category
+        self._is_directed = is_directed
+
+    def is_directed(self):
+        return self.is_directed
+
+    def get_nodes(self):
+        return self._first_node, self._second_node
